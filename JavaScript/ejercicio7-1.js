789351284dbecb9ca454221f6f09f520d10e19d5
bpizzagalli <brunopizzagalli1@gmail.com>
2022-08-23 08:10:33 -0300

    subo ejercicio 7-1

---
+++ JavaScript/ejercicio7-1.js
@@ -0,0 +1,9 @@
+const arrayFamilia=['Roberto', 'Rosana', 'Rocio', 'Franco', 'Bruno'];
+const setFamilia= new Set(arrayFamilia);
+console.log(setFamilia);
+setFamilia.add('Bruno');
+console.log(setFamilia);
+setFamilia.add('Javascript');
+console.log(setFamilia);
+
+
