22b1ff47fcbc7c2c3e89d6ea3ab13f96296215e0
bpizzagalli <brunopizzagalli1@gmail.com>
2022-09-05 02:44:24 -0300

    subo ejercicio 13

---
+++ JavaScript/ejercicio13/index.js
@@ -0,0 +1,6 @@
+const comSimples="Utiliza los tres estilos de comillas de forma alterna (comillas simples, dobles, backticks)"
+const comDobles="Utiliza los tres estilos de comillas de forma alterna (comillas simples, dobles, backticks)"
+const comBackticks="Utiliza los tres estilos de comillas de forma alterna (comillas simples, dobles, backticks)"
+console.log(comSimples)
+console.log(comDobles)
+console.log(comBackticks)
